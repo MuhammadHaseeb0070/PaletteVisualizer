c68696feadc74be18807f376fef603a746221596
muhammadhaseeb0070 <muhammadhaseeb0070@gmail.com>
2024-07-31 13:27:34 +0500

    made closeColorPicker BUg Fix

--- src/components/ColorPicker/ColorPicker.jsx
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -3,10 +3,10 @@ import { ChromePicker } from 'react-color';
 import './colorpicker.css';
 import { IoCloseOutline } from "react-icons/io5";
 
-const ColorPicker = ({ color, onChange, closeColorPicker }) => {
+const ColorPicker = ({ color, onChange, closeColorPicker,setcloseColorPicker }) => {
   return (
     <div className="color-picker-popup">
-      <IoCloseOutline onClick={closeColorPicker} className='colorPickerCloseButton' />
+      <IoCloseOutline onClick={()=>{setcloseColorPicker(!closeColorPicker)}} className='colorPickerCloseButton' />
       <ChromePicker color={color} onChange={onChange} />
     </div>
   );
